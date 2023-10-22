193040685f0fa2f5b81a44a7e46492d587250971
Brahimhz <Brahimhz>
2023-10-22 16:40:01 +0400

    UnitOfWork Command + Handler

---
+++ MyIAM.Core/Infrastructure/Commands/UnitOfWorkCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIAM.Core.Infrastructure.Commands
+{
+    public record UnitOfWorkCommand() : IRequest<Task>;
+}
---
+++ MyIAM.Core/Infrastructure/Handlers/UnitOfWorkHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MyIAM.Core.Databases.Contracts;
+using MyIAM.Core.Infrastructure.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIAM.Core.Infrastructure.Handlers
+{
+    public class UnitOfWorkHandler : IRequestHandler<UnitOfWorkCommand, Task>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UnitOfWorkHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public Task<Task> Handle(UnitOfWorkCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_unitOfWork.CompleteAsync());
+        }
+    }
+}
